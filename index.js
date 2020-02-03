5ccf1044245e763d560cad4c43edd34d7fcc6e2c
Maxim Tsoy <maks.tsoy@gmail.com>
2020-02-03 08:32:43 -0600

    Wait for a navigate to finish before killing the browser

--- index.js
+++ index.js
@@ -8,31 +8,45 @@ var SeleniumBrowser = function (baseBrowserDecorator, args, logger) {
   baseBrowserDecorator(this);
 
   this.name = 'selenium for ' + args.browserName;
+  this.navigated = false;
 
   this._start = function(url) {
     log.info('Selenium browser started at http://' + args.config.hostname + ':' + args.config.port + args.config.path);
     remote(args.config).then(browser => {
       self.browser = browser;
-      browser.url(url);
+      return browser.url(url);
+    }).then(() => {
+      self.navigated = true;
+      log.info(self.name + ' navigated to the Karma URL');
     }).catch(e => {
+      self.navigated = true;
       log.info('Error retrieving a selenium instance: ', e.message);
     });
   };
 
-  this.on('kill', async function(done){
+  this.on('kill', function(done) {
     if(!self.browser){
       process.nextTick(done);
       return;
     }
 
-    try {
-      await self.browser.deleteSession();
-      log.info('Browser closed');
-    } catch (error) {
-      log.error('Browser closed with error:\n' + error.message + '\n' + error.stack);
-    }
-    self._done();
-    done();
+    // need to delay the end request in case the navigation request has not completed yet
+    const delayedEnd = () => {
+      if (self.navigated) {
+        self.browser.deleteSession().then(() => {
+          log.info('Browser closed');
+          self._done();
+          done();
+        }).catch((error) => {
+          log.error('Browser closed with error:\n' + error.message + '\n' + error.stack);
+          self._done();
+          done();
+        });
+      } else {
+        setTimeout(delayedEnd, 100);
+      }
+    };
+    delayedEnd();
   });
 };
 
